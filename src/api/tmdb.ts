09fc5bea8de3e180a8f4bc0688cec0e4795ddd09
leonard800 <leonardabimanyu@gmail.com>
2025-06-07 17:06:25 +0700

    feat: setting up API for the now playing and top rated section using axios

---
+++ src/api/tmdb.ts
@@ -0,0 +1,23 @@
+import axios from 'axios';
+
+const API_KEY = import.meta.env.VITE_TMDB_API_KEY;
+const BASE_URL = 'https://api.themoviedb.org/3';
+
+export const fetchNowPlaying = async () => {
+    const response = await axios.get(`${BASE_URL}/movie/now_playing?api_key=${API_KEY}`);
+    return response.data;
+}
+
+export const fetchTopRated = async () => {
+    const response = await axios.get(`${BASE_URL}/movie/top_rated?api_key=${API_KEY}`);
+    return response.data;
+}
+
+export const fetchPopular = async () => {
+    const response = await axios.get(`${BASE_URL}/movie/popular?api_key=${API_KEY}`);
+    return response.data;
+}
+
+export const getImageUrl = (path: string) => {
+    return `https://image.tmdb.org/t/p/w500/${path}`
+}
--- src/components/NowPlaying.tsx
+++
@@ -1,9 +0,0 @@
-"use client";
-// eslint-disable-next-line
-import React from "react";
-
-const NowPlaying: React.FC => {
-    return <div>NowPlaying</div>;
-};
-
-export default NowPlaying;
